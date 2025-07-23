fad30b1d02eff67f958619e8654b1c7b9d8e69ba
Anjali Arora <anjalisharma888999@gmail.com>
2025-07-23 21:07:32 +0530

    Update form.vue

--- src/pages/service-zone/form.vue
+++ src/pages/service-zone/form.vue
@@ -475,6 +475,7 @@
                 placeholder="#000"
                 class="w-28"
               />
+              <VaColorInput v-model="restaurantData.textColor" label="Text Color" placeholder="#000" class="w-28" />
               <VaColorInput v-model="restaurantData.headerColor" label="Header Color" placeholder="#000" class="w-28" />
               <VaColorInput v-model="restaurantData.footerColor" label="Footer Color" placeholder="#000" class="w-28" />
               <VaSwitch
@@ -787,6 +788,7 @@ export default {
         primaryColor: '',
         secondaryColor: '',
         backgroundColor: '',
+        textColor: '',
         headerColor: '',
         footerColor: '',
         fontFamily: '',
@@ -1259,6 +1261,7 @@ export default {
         primaryColor: this.restaurantData.primaryColor,
         secondaryColor: this.restaurantData.secondaryColor,
         backgroundColor: this.restaurantData.backgroundColor,
+        textColor: this.restaurantData.textColor,
         headerColor: this.restaurantData.headerColor,
         footerColor: this.restaurantData.footerColor,
         headerUrl: this.restaurantData.headerUrl,
