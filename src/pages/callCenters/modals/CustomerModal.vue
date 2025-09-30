e464f4429d4bf3f66df7293144def8e220fd9516
Anjali Arora <anjalisharma888999@gmail.com>
2025-09-30 16:55:44 +0530

    Update CustomerModal.vue

--- src/pages/callCenters/modals/CustomerModal.vue
+++ src/pages/callCenters/modals/CustomerModal.vue
@@ -476,6 +476,7 @@ async function addOrUpdateCustomerDetails() {
     notifications: notifications.value,
     customerNote: '',
     addressNote: '',
+    optOutDataSaving: !isTick.value,
   }
   try {
     let response
